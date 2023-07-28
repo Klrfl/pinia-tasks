52352fea228eb9e984488237f5d023b55f995694
Efraim Munthe <efrayanglain@gmail.com>
2023-07-28 21:38:34 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img
+      alt="Pinia logo"
+      class="logo"
+      src="./assets/pinia.svg"
+      width="125"
+      height="125" />
+
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <h1>Pinia tasks</h1>
+    <p>I'm learning Pinia!!</p>
+  </main>
+</template>
