caced72b04895d92dbaa0afae3ae0d1d4bd92622
Efraim Munthe <efrayanglain@gmail.com>
2023-07-29 19:53:40 +0700

    make new component

---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  task: Object,
+});
+</script>
+
+<template>
+  <li class="task">
+    <p class="task__text">
+      {{ task.text }}
+    </p>
+    <button class="btn task__delete-btn">
+      <i class="material-icons">delete</i>
+    </button>
+    <button class="btn task__favorite-btn">
+      <i class="material-icons">favorite</i>
+    </button>
+  </li>
+</template>
