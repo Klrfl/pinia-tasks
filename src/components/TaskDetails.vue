cca794a9e9ea64db732d7b4d800d83a7a8aff8a2
Efraim Munthe <efrayanglain@gmail.com>
2023-07-30 20:55:56 +0700

    a splash of color

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -9,7 +9,7 @@ const taskStore = useTaskStore();
 </script>
 
 <template>
-  <li class="task">
+  <li :class="{ 'task--complete': task.isDone }" class="task">
     <p class="task__text">
       {{ task.text }}
     </p>
@@ -21,12 +21,14 @@ const taskStore = useTaskStore();
       </button>
 
       <button
+        :class="{ 'task__favorite-btn--complete': task.isDone }"
         class="btn task__done-btn"
         @click="taskStore.completeTask(task.id)">
         <i class="material-icons">done</i>
       </button>
 
       <button
+        :class="{ 'task__favorite-btn--fav': task.isFav }"
         class="btn task__favorite-btn"
         @click="taskStore.favTask(task.id)">
         <i class="material-icons">favorite</i>
@@ -39,10 +41,29 @@ const taskStore = useTaskStore();
 .task {
   list-style: none;
   outline: 2px solid var(--color-border);
+  margin-block-end: 0.5rem;
 
   display: flex;
   align-items: center;
   gap: 0.5rem;
+
+  position: relative;
+}
+
+.task--complete {
+  text-decoration: line-through;
+}
+
+.task::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  width: 0.25rem;
+  transition: background-color 250ms ease;
+}
+
+.task--complete::before {
+  background: var(--vt-c-accent);
 }
 
 .task__text {
@@ -57,7 +78,15 @@ const taskStore = useTaskStore();
 }
 
 .task .btn {
-  background: var(--vt-c-accent);
-  padding: 0.5em;
+  padding: 1em;
+  transition: color 0.4s ease;
+}
+
+.task__favorite-btn--complete {
+  color: var(--vt-c-accent);
+}
+
+.task__favorite-btn--fav {
+  color: var(--vt-c-green);
 }
 </style>
