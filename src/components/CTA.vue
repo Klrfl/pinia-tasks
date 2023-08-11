e5260dd0f6ef347b5d3d61cb97898e9d00bdae0e
Efraim Munthe <efrayanglain@gmail.com>
2023-08-11 09:54:38 +0700

    add new component

---
+++ src/components/CTA.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn btn--cta">
+    <slot>Click me</slot>
+  </button>
+</template>
+
+<style scoped>
+.btn--cta {
+  outline: 1px solid var(--vt-c-accent);
+  margin-block: 1rem;
+}
+</style>
